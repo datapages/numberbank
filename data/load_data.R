746517f241ce605d9a248c97b4999c71375f6586
Mika Braginsky <mika.br@gmail.com>
2024-03-11 17:40:46 -0400

    switch to redivis data, add embeds

--- data/load_data.R
+++ data/load_data.R
@@ -4,18 +4,19 @@ library(readr)
 library(stringr)
 library(forcats)
 
-kl_data_processed <- read_csv("data/kl_data_processed.csv")
-trial_level_processed_data <- read_csv("data/trial_level_processed_data.csv")
-highest_count <- read_csv("data/highest_count.csv")
+kl_data_processed <- read_csv("pre/kl_data_processed.csv")
+trial_level_processed_data <- read_csv("pre/trial_level_processed_data.csv")
+highest_count <- read_csv("pre/highest_count.csv")
 
-country <- read_csv("data/country.csv")
-country_by_subject <- read_csv("data/country_by_subj.csv")
+country <- read_csv("pre/country.csv")
+country_by_subject <- read_csv("pre/country_by_subj.csv")
 
 hc <- highest_count |>
   select(dataset_id = Experiment, subject_id = SID, hc = highest_count) |>
   group_by(dataset_id, subject_id) |>
   summarise(hc = max(hc)) |> # take each kid's max hc (only a few multiples)
   ungroup()
+write_csv(hc, "post/hc.csv", na = "")
 
 kld <- kl_data_processed |>
   left_join(country) |>
@@ -34,25 +35,7 @@ kld <- kl_data_processed |>
   mutate(kl = kl |> factor() |> fct_relabel(\(l) paste0(l, "-knower")), # add "-knower" to kl
          kl_subset = if_else(kl == "CP-knower", kl, "Subset-knower")) |> # code subset-knowers
   left_join(hc)
-  
-# cp_val <- "100"
-# ss_val <- "10"
-# kld_deduped <- kld |>
-#   mutate(klm = case_when(kl == "CP" ~ cp_val,
-#                          kl == "Subset" ~ ss_val,
-#                          TRUE ~ kl),
-#          klm = as.numeric(klm)) |>
-#   group_by(dataset_id, language, country, method, subject_id, age) |>
-#   summarise(kls = list(kl),
-#             klm = max(klm)) |>
-#   ungroup() |>
-#   mutate(klm = case_when(klm == cp_val ~ "CP",
-#                          klm == ss_val ~ "Subset",
-#                          TRUE ~ as.character(klm)))
-
-  
-# kl_data_processed |> group_by(Experiment, Subject, method, Age_months, KL) |> filter(n() > 1)
-# kld |> group_by(dataset_id, subject_id, method, age, kl) |> filter(n() > 1)
+write_csv(kld, "post/kld.csv", na = "")
 
 kls <- kld |> select(-hc)
 
@@ -66,3 +49,4 @@ td <- trial_level_processed_data |>
   mutate(method = method |>
            fct_collapse("non-titrated" = c("Non-titrated", "nontitrated"))) |>
   left_join(kls)
+write_csv(td, "post/td.csv", na = "")
